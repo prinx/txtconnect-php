6e5f68d973e97d888d42985738eaa4a61105fe70
Prince Dorcis <damien.dorcis@yahoo.com>
2021-04-29 14:17:44 +0000

    feat: add SmsResponseBagAbstract

---
+++ src/Txtconnect/Abstracts/SmsResponseBagAbstract.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Prinx\Txtconnect\Abstracts;
+
+use Prinx\Txtconnect\Contracts\SmsResponseBagInterface;
+
+abstract class SmsResponseBagAbstract implements SmsResponseBagInterface
+{
+}
