d4350977a9d8b7d3da1ea89ab1dea501a5f30da8
Prince Dorcis <damien.dorcis@yahoo.com>
2021-04-30 18:04:44 +0000

    feat: inbox class using InboxItem to deliver items

--- src/Txtconnect/Inbox.php
+++ src/Txtconnect/Inbox.php
@@ -6,34 +6,81 @@ use Prinx\Txtconnect\Abstracts\InboxAbstract;
 
 class Inbox extends InboxAbstract
 {
-    protected $inbox = null;
+    /**
+     * Inbox items in object form.
+     *
+     * @var array[]|null
+     */
+    protected $raw = null;
+
+    /**
+     * Inbox items in object form.
+     *
+     * @var InboxItem[]
+     */
+    protected $items = [];
+
     protected $fetched = false;
 
+    /**
+     * Get inbox content fron API.
+     *
+     * @return $this
+     */
     public function fetch()
     {
         $response = $this->request(self::endpoint());
 
-        $this->inbox = $response->toArray();
+        $this->raw = $response->toArray();
+        $this->items = [];
         $this->fetched = true;
 
         return $this;
     }
 
+    /**
+     * Get Inbox endpoint.
+     *
+     * @return string
+     */
     public static function endpoint()
     {
         return Endpoint::inbox();
     }
 
+    /**
+     * All the items in the inbox.
+     *
+     * @return InboxItem[]
+     */
     public function all()
     {
-        return $this->nth();
+        $allRaw = $this->nth();
+
+        foreach ($allRaw as $index => $item) {
+            if (!isset($this->items[$index])) {
+                $this->items[$index] = new InboxItem($item, $index);
+            }
+        }
+
+        return $this->items;
     }
 
+    /**
+     * Get first inbox item.
+     *
+     * @return InboxItem
+     */
     public function first()
     {
         return $this->nth(1);
     }
 
+    /**
+     * Get last inbox item.
+     *
+     * @return InboxItem
+     */
     public function last()
     {
         return $this->nth($this->count() - 1);
@@ -41,7 +88,7 @@ class Inbox extends InboxAbstract
 
     public function nth($number = '', $key = '')
     {
-        if (is_null($this->inbox) || !$this->fetched) {
+        if (is_null($this->raw) || !$this->fetched) {
             $this->fetch();
         }
 
@@ -49,7 +96,15 @@ class Inbox extends InboxAbstract
             return $this->payload;
         }
 
-        return $key ? $this->inbox[$number][$key] : $this->inbox[$number];
+        if ($key) {
+            return $this->raw[$number][$key];
+        }
+
+        if (!isset($this->items[$number])) {
+            $this->items[$number] = new InboxItem($this->raw[$number], $number);
+        }
+
+        return $this->items[$number];
     }
 
     public function count()
