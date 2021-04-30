0891683450156380a01b9dbda0f3d210779ad754
Prince Dorcis <damien.dorcis@yahoo.com>
2021-04-30 18:28:51 +0000

    feat: add parameters to exceptions

--- src/Txtconnect/Exceptions/UndefinedInboxItemContentException.php
+++ src/Txtconnect/Exceptions/UndefinedInboxItemContentException.php
@@ -4,4 +4,8 @@ namespace Prinx\Txtconnect\Exceptions;
 
 class UndefinedInboxItemContentException extends \InvalidArgumentException
 {
+    public function __construct($number, $key)
+    {
+        parent::__construct('Key '.$key.' does not exist in content of inbox item number '.$number);
+    }
 }
--- src/Txtconnect/Exceptions/UndefinedInboxItemException.php
+++ src/Txtconnect/Exceptions/UndefinedInboxItemException.php
@@ -4,4 +4,8 @@ namespace Prinx\Txtconnect\Exceptions;
 
 class UndefinedInboxItemException extends \InvalidArgumentException
 {
+    public function __construct($number)
+    {
+        parent::__construct('Item number '.$number.' does not exist in inbox.');
+    }
 }
