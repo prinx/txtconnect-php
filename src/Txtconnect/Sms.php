d8e7fc47d96487d9c981a90aadd36550c9a3b1ba
Prince Dorcis <damien.dorcis@yahoo.com>
2021-04-30 16:00:15 +0000

    refactor: converting unicode to int, just to comply with documentation

--- src/Txtconnect/Sms.php
+++ src/Txtconnect/Sms.php
@@ -129,7 +129,7 @@ class Sms extends SmsAbstract
             'from' => $this->getFrom(),
             'sms' => $this->getSmsString($sms),
             'response' => 'json',
-            'unicode' => $this->getIsUnicode(),
+            'unicode' => intval($this->getIsUnicode()),
         ];
     }
 
