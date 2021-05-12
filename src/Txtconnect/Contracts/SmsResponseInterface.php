b089c5bc9b0a20e6d9e10dae75a6b74bfb8194a6
Prince Dorcis <damien.dorcis@yahoo.com>
2021-05-12 05:38:16 +0000

    feat: fill SmsResponseInterface and add phpDoc to SmsResponse

--- src/Txtconnect/Contracts/SmsResponseInterface.php
+++ src/Txtconnect/Contracts/SmsResponseInterface.php
@@ -4,4 +4,100 @@ namespace Prinx\Txtconnect\Contracts;
 
 interface SmsResponseInterface
 {
+    /**
+     * Has SMS been forwarded to Telco successfully for delivering onto the user's phone?
+     *
+     * This does not mean the user has received the SMS. The reception of SMS by user still depends
+     * on the availability of the Telco processing the SMS.
+     *
+     * To know if the user has actually received the SMS, you need to check the status of the SMS.
+     *
+     * @return bool
+     */
+    public function isOk();
+
+    /**
+     * API code of the SmsResponse.
+     *
+     * @return string|int|null
+     */
+    public function getCode();
+
+    /**
+     * Batch number to which belong the SMS.
+     * Returns null if an error happened and SMS did not reach TxtConnect for processing.
+     *
+     * @return string|null
+     */
+    public function getBatchNumber();
+
+    /**
+     * User name of the account used to send the SMS.
+     * Returns null if an error happened and SMS did not reach TxtConnect for processing.
+     *
+     * @return string|null
+     */
+    public function getUserName();
+
+    /**
+     * URL to check the status of this SMS.
+     * Check if user has received SMS.
+     * Returns null if an error happened and SMS did not reach TxtConnect for processing.
+     *
+     * @return string|null
+     */
+    public function getStatusCheckUrl();
+
+    /**
+     * Available balance after SMS has been sent.
+     * Returns null if an error happened and SMS did not reach TxtConnect for processing.
+     *
+     * @return int|null
+     */
+    public function getBalance();
+
+    /**
+     * Message attached to the status of the SMS request.
+     *
+     * @return string|null
+     */
+    public function getMessage();
+
+    /**
+     * Error of the SMS request.
+     *
+     * @return string|null
+     */
+    public function getError();
+
+    /**
+     * Actual number used to send the SMS.
+     *
+     * @return string
+     */
+    public function getParsedNumber();
+
+    /**
+     * Original number user sent the SMS to.
+     *
+     * @return string
+     */
+    public function getOriginalNumber();
+
+    /**
+     * Raw response of the request.
+     *
+     * @return string
+     */
+    public function getRawResponse();
+
+    /**
+     * Get full response body or key of body.
+     *
+     * @param string|null $key
+     * @param mixed|null  $default
+     *
+     * @return mixed
+     */
+    public function getResponse($key = null, $default = null);
 }
--- src/Txtconnect/SmsResponse.php
+++ src/Txtconnect/SmsResponse.php
@@ -40,6 +40,9 @@ class SmsResponse extends SmsResponseAbstract
         $this->response = $response->toArray();
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function isOk()
     {
         if (is_null($this->isOk)) {
@@ -49,56 +52,89 @@ class SmsResponse extends SmsResponseAbstract
         return $this->isOk;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getCode()
     {
         return $this->getResponse('code', self::ERROR);
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getBatchNumber()
     {
         return $this->getResponse('batch_no', null);
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getUserName()
     {
         return $this->getResponse('user', null);
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getStatusCheckUrl()
     {
         return $this->getResponse('status_check_url', null);
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getBalance()
     {
         return $this->getResponse('balance', null);
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getMessage()
     {
         return $this->getResponse('message', $this->error);
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getError()
     {
         return $this->error;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getParsedNumber()
     {
         return $this->parsedNumber;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getOriginalNumber()
     {
         return $this->originalNumber;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getRawResponse()
     {
         return $this->rawResponse;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getResponse($key = null, $default = null)
     {
         return $key ? $this->response[$key] ?? $default : $this->response;
