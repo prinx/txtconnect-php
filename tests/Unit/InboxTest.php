dbee4f681b24973cb3ed987fbba25c79151cec44
Prince Dorcis <damien.dorcis@yahoo.com>
2021-05-16 05:36:59 +0000

    feat: add inbox test

---
+++ tests/Unit/InboxTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+
+class InboxTest extends TestCase
+{
+    public function testGettingInbox()
+    {
+        $this->assertTrue(1 === 1);
+    }
+}
