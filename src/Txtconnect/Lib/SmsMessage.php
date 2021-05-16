b9efe1eac6767ce98872862685425fad3004c760
Prince Dorcis <damien.dorcis@yahoo.com>
2021-05-16 15:49:46 +0000

    feat: add SmsMessage->isDelivered

--- src/Txtconnect/Lib/SmsMessage.php
+++ src/Txtconnect/Lib/SmsMessage.php
@@ -15,6 +15,16 @@ class SmsMessage
     }
 
     /**
+     * Has sms been delivered successfully?
+     *
+     * @return bool
+     */
+    public function isDelivered()
+    {
+        return strtolower($this->status()) === 'message delivered successfully';
+    }
+
+    /**
      * @return int
      */
     public function id()
