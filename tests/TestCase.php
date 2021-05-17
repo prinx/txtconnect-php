d4d977ebd0546f662b8f88874521e78b77963355
Prince Dorcis <damien.dorcis@yahoo.com>
2021-05-17 07:40:27 +0000

    fix: creating env file if no available

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,10 +3,29 @@
 namespace Tests;
 
 use PHPUnit\Framework\TestCase as BaseTestCase;
+use function Prinx\Dotenv\loadEnv;
 
 /**
  * Base Test case.
  */
 abstract class TestCase extends BaseTestCase
 {
+    public function setUp(): void
+    {
+        $this->loadEnv(realpath(__DIR__.'/../').'/.env');
+    }
+
+    public function createEnvIfNotExist($path)
+    {
+        if (!file_exists($path)) {
+            file_put_contents($path, '');
+        }
+    }
+
+    public function loadEnv($env)
+    {
+        $this->createEnvIfNotExist($env);
+
+        loadEnv($env);
+    }
 }
